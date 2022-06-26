ddd1ec4d253e69eed8e2650dff684a0dce84bdac
nequals30 <nequals30@gmail.com>
2022-06-26 11:38:31 -0500

    minor modifications to get_dougStats_data documentation

--- src/FantasyBaseballTools.jl
+++ src/FantasyBaseballTools.jl
@@ -5,18 +5,21 @@ using Dates, HTTP, CSV, DataFrames
 export get_dougStats_data
 
 """
-    get_dougStats_data(dt::Date, batterPitcher::AbstractString="batter") -> DataFrame
+    get_dougStats_data(dt::Date, batterOrPitcher::AbstractString="batter") -> DataFrame
 
 Download player data from dougstats.com and returns a DataFrame.
 
+# Arguments
+* `dt`: The as-of-date of the data
+* `batterOrPitcher`: "batter" or "pitcher" data
 
 # Examples
 ```jldoctest
 julia> get_dougStats_data(Date(2022,06,19),"pitcher")
 ```
 """
-function get_dougStats_data(dt::Date, batterPitcher::AbstractString="batter")
-    url = "http://dougstats.com/" * Dates.format(dt,"yyyy") * "Data/" * Dates.format(dt,"mmdd") * lowercase(batterPitcher[1])
+function get_dougStats_data(dt::Date, batterOrPitcher::AbstractString="batter")
+    url = "http://dougstats.com/" * Dates.format(dt,"yyyy") * "Data/" * Dates.format(dt,"mmdd") * lowercase(batterOrPitcher[1])
     r = HTTP.request("GET",url)
     body_contents = r.body[7:(end-8)]
     body_csv = CSV.File(IOBuffer(body_contents), delim=' ', ignorerepeated=true)
